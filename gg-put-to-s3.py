28cd335391999df0a29d801b9a9aed4b889956b7
KSMN455 <kusomine455@gmail.com>
2020-04-15 08:35:18 +0000

    Modified python script

--- gg-put-to-s3.py
+++ gg-put-to-s3.py
@@ -16,8 +16,6 @@ def gg_put_to_s3():
         cv2.imwrite(file_name, frame)
         s3_key = "/png/" + file_name
         s3.meta.client.upload_file(file_name, "gg-png-119463424712", s3_key)
-    except Exception as e:
-        print(e)
     finally:
         if os.path.exists(file_name):
             os.remove(file_name)
