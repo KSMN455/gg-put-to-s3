a250617a1fe8bdfee8163f89751437600455b19b
KSMN455 <kusomine455@gmail.com>
2020-04-15 05:55:45 +0000

    Modified python script

--- gg-put-to-s3.py
+++ gg-put-to-s3.py
@@ -4,28 +4,24 @@ import os
 import uuid
 import numpy as np
 
+from threading import Timer
+
 s3 = boto3.resource('s3')
 capture = cv2.VideoCapture(0)
 
 def gg_put_to_s3():
-    frame_count = 0
-    while True:
-        try:
-            ret, frame = capture.read()
-            if not frame_count < 900:
-                break
-            else:
-                frame_count = 0
-                file_name = str(uuid.uuid4()) + ".png"
-                cv2.imwrite(file_name, frame)
-            frame_count += 1
-        except Exception as e:
-            raise e
-        finally:
-            if os.path.exists(file_name):
-                s3_key = "/png/" + file_name
-                s3.meta.client.upload_file(file_name, "gg-png-119463424712", s3_key)
-                os.remove(file_name)
+    try:
+        ret, frame = capture.read()
+        file_name = str(uuid.uuid4()) + ".png"
+        cv2.imwrite(file_name, frame)
+        s3_key = "/png/" + file_name
+        s3.meta.client.upload_file(file_name, "gg-png-119463424712", s3_key)
+    except Exception as e:
+        raise e
+    finally:
+        if os.path.exists(file_name):
+            os.remove(file_name)
+    Timer(30, gg_put_to_s3).start()
 
 gg_put_to_s3()
 
